d5de27dbb1d4529de5ef17ec6aa595d1f41fcb89
Aleksandr Kolotov <kolotov2004@list.ru>
2026-09-02 16:30:16 +0300

    add user schemas and foreign key to task model

--- app/schemas.py
+++ app/schemas.py
@@ -17,3 +17,16 @@ class TaskResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+
+    class Config:
+        from_attributes = True
