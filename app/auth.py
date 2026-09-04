a25cb30555394bf80ad9feb3ceed0262dc7f2c34
Aleksandr Kolotov <kolotov2004@list.ru>
2026-09-04 14:25:56 +0300

    add get_current_user for jwt auth

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,13 @@
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from app import crud
+from app.database import get_db
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
 
 load_dotenv()
 
@@ -13,6 +18,8 @@ ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 
 def hash_password(password: str) -> str:
@@ -28,3 +35,19 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({'exp': expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials')
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get('sub')
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = crud.get_user(db, int(user_id))
+    if user is None:
+        raise credentials_exception
+    return user
