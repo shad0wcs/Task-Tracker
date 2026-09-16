191edbe2607bb94768ff0e43cde75c9b637c9c98
Aleksandr Kolotov <kolotov2004@list.ru>
2026-09-16 16:01:35 +0300

    add next testes: login, login with wrong pass, create task

--- test_main.py
+++ test_main.py
@@ -34,3 +34,51 @@ def test_register_user():
     data = response.json()
     assert data['email'] == 'testuser@example.com'
     assert 'id' in data
+
+
+def test_login_user():
+    client.post('/register', json={
+        "email": "testuser1@example.com",
+        "password": "testpass1123"
+    })
+    response = client.post('/login', json={
+        "email": "testuser1@example.com",
+        "password": "testpass1123" 
+    })
+    assert response.status_code == 200
+    data = response.json()
+    assert 'access_token' in data
+
+
+def test_login_wrong_password():
+    client.post('/register', json={
+        "email": "testuser_wrongpassword@example.com",
+        "password": "password"
+    })
+    response = client.post('/login', json={
+        "email": "testuser_wrongpassword@example.com",
+        "password": "wrong_password" 
+    })
+    assert response.status_code == 401
+
+
+def test_create_task():
+    client.post('/register', json={
+        "email": "createtask@example.com",
+        "password": "testpass1123"
+    })
+    login_response = client.post('/login', json={
+        "email": "createtask@example.com",
+        "password": "testpass1123" 
+    })
+    token = login_response.json()['access_token']
+
+    response = client.post('/tasks', 
+        json={'title': 'Test Task'},
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data['title'] == 'Test Task'
+    
